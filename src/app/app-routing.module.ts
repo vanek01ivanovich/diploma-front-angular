81d08ecfb91d58047ae46621e395d55252bceb9f
Ivan <ivan.kuchkov17@gmail.com>
2021-02-04 18:30:45 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {AuthenticationComponent} from './authentication/authentication.component';
 
 const routes: Routes = [
+  { path: 'login', component: AuthenticationComponent},
 ];
 
 @NgModule({
