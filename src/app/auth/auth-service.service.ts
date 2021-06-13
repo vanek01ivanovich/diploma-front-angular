0c935e7387550a1e38a6c989f8150d85f93d88a6
Ivan <ivan.kuchkov17@gmail.com>
2021-06-13 21:28:07 +0300

    last commit

--- src/app/auth/auth-service.service.ts
+++ src/app/auth/auth-service.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthLoginInfo} from './auth-login-info';
 import {Observable} from 'rxjs';
 import {JwtResponse} from './jwt-response';
+import {environment} from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -15,8 +16,8 @@ const httpOptions = {
 export class AuthServiceService {
 
   constructor(private http: HttpClient) { }
-
-  private loginUrl = 'http://localhost:8080/login';
+  private url = `${environment.url}`;
+  private loginUrl = this.url + 'login';
 
   login(credentials: AuthLoginInfo): Observable<JwtResponse>{
     return this.http.post<JwtResponse>(this.loginUrl, credentials, httpOptions);
